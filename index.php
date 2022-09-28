0adb92854b417925a175c795375d686608c4ebb9
Patrick Mutugi Muriithi <PMMURIITHI@safaricom.co.ke>
2022-09-28 09:00:00 +0300

    checkout

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE HTML>
 <html>  
 <head>
-  <title>Bootstrap 5 Example</title>
+  <title>Checkout Page</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
